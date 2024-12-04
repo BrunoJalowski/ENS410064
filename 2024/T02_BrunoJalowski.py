6186127f591491348cfe091ee248804839972d5b
BrunoJalowski <bruno.jalowski@gmail.com>
2024-12-04 09:27:25 -0300

    Update T02_BrunoJalowski.py

--- 2024/T02_BrunoJalowski.py
+++ 2024/T02_BrunoJalowski.py
@@ -5,15 +5,16 @@ import xarray as xr
 import rioxarray as rxr
 import pandas as pd
 from pathlib import Path
+import matplotlib.pyplot as plt
 
 #%%2
 # Abrindo arquivos
-pathDS = Path('/home/lcqar/ENS410064/2024/dados_entrada/congonhas')
+pathDS = Path(r"C:\Users\bruno\Desktop\UFSC\2024.2\ENS410064\2024\dados_entrada\congonhas_clipped")
 files = [i for i in pathDS.rglob('*.tif')]
 
 #%%3
 # Carregandi DataArrays
-xdas = [rxr.open_rasterio(file) for file in files]
+xdas = [rxr.open_rasterio(file, chunks='auto') for file in files]
 xdas
 
 # Criando índices de tempo a partir dos nomes dos arquivos
@@ -40,8 +41,10 @@ ds = xr.concat(xdas, dim=time_indexes)
 ds = ds.to_dataset(name='color')
 ds
 del xdas 
-x = ds.x[:]
-y = ds.y[:]
+
+
+# x = ds.x[:]
+# y = ds.y[:]
 #%%5
 
 # Cores de limites
@@ -69,7 +72,7 @@ del r_values, g_values, b_values
 
 
 #%%5.1
-def rgba_to_class(arr: np.array) -> int:
+def rgba_to_class(b0, b1, b2) -> int:
     #print(arr)
     # Repete np.array p/ nº de vezes do rgb_values
     # Compara com rgb_values
@@ -82,52 +85,72 @@ def rgba_to_class(arr: np.array) -> int:
     #        return rgb_values.index(rgb_value)
     
     for i, ref in enumerate(rgb_values):
-        if np.array_equal(arr, ref):
+        if np.array_equal(np.array([b0,b1,b2]), ref):
             return i
     return 0
 
-print(rgba_to_class([255,35,35]))
+print(rgba_to_class(255,35,35))
 
 #%%6
 
 # ds.reduce(rgba_to_class, dim='band', keep_attrs =True, keepdims=True)
-nds = xr.apply_ufunc(rgba_to_class, ds, input_core_dims=[['band']], vectorize=True)
+nds = xr.apply_ufunc(
+    rgba_to_class,
+    ds.isel(band=0),
+    ds.isel(band=1),
+    ds.isel(band=2),
+    input_core_dims=[[],[],[]],
+    vectorize=True,
+    dask="parallelized",
+    output_dtypes=[np.int32]
+)
+
 
 #%%
-testAll=[]
-for classification, rgb_value in rgb_values.items():
-    testband = (ds[:,0,:,:]==rgb_value[0]) & (ds[:,1,:,:]==rgb_value[1]) &\
-        (ds[:,2,:,:]==rgb_value[2])
-    testAll.append(testband)
 
-del testband
+nds.compute()
+
+#%%
+nds.to_netcdf(r'C:\Users\bruno\Desktop\UFSC\2024.2\ENS410064\2024\dados_entrada\Vel_classes.nc')
+#%%
+
+nds.color.plot(col='time')
+
+#%%
+# testAll=[]
+# for classification, rgb_value in rgb_values.items():
+#     testband = (ds[:,0,:,:]==rgb_value[0]) & (ds[:,1,:,:]==rgb_value[1]) &\
+#         (ds[:,2,:,:]==rgb_value[2])
+#     testAll.append(testband)
+
+# del testband
 
 #%%
 
-testAll = np.concat(testAll)
-testAll = xr.concat(np.concat(testAll).reshape(len(limits),ds.shape[0],
-                ds.shape[2],ds.shape[3]), dim=time_indexes)
+# testAll = np.concat(testAll)
+# testAll = xr.concat(np.concat(testAll).reshape(len(limits),ds.shape[0],
+#                 ds.shape[2],ds.shape[3]), dim=time_indexes)
 
-testAll = testAll.reshape(len(limits),ds.shape[0],
-                ds.shape[2],ds.shape[3])
+# testAll = testAll.reshape(len(limits),ds.shape[0],
+#                 ds.shape[2],ds.shape[3])
 
-del classification
+# del classification
 
 #%%7
-# Criando DataArray
-testAll = xr.DataArray(testAll, 
-                          coords={'y': ds.y[:],'x': ds.x[:],'time': ds.time[:]}, 
-                          dims=['class',"time","y","x"],
-                          name = 'testAll')
+# # Criando DataArray
+# testAll = xr.DataArray(testAll, 
+#                           coords={'y': ds.y[:],'x': ds.x[:],'time': ds.time[:]}, 
+#                           dims=['class',"time","y","x"],
+#                           name = 'testAll')
 
-testAll.rxr.write_crs('EPSG:4326', inplace=True)
+# testAll.rxr.write_crs('EPSG:4326', inplace=True)
 
-del ds
+# del ds
 #%%8
 # Ler shapefile de ruas do open street maps da Geofabrik
 import geopandas as gpd
 
-pathSHP = "/home/lcqar/ENS410064/2024/dados_entrada/roads_sudeste/gis_osm_roads_free_1.shp"
+pathSHP = r"C:\Users\bruno\Desktop\UFSC\2024.2\ENS410064\2024\dados_entrada\roads_sudeste\gis_osm_roads_free_1.shp"
 gdf = gpd.read_file(pathSHP)
 
 gdf
@@ -138,23 +161,26 @@ gdf.crs
 
 #%%
 # DEFINIR AS COORDENADAS DOS LIMITES DO DATASET
-x_min = testAll_xr.coords['x'].min().values
-x_max = testAll_xr.coords['x'].max().values
-y_min = testAll_xr.coords['y'].min().values
-y_max = testAll_xr.coords['y'].max().values
+x_min = nds.coords['x'].dropna(dim='x').min().values
+x_max = nds.coords['x'].dropna(dim='x').max().values
+y_min = nds.coords['y'].dropna(dim='y').min().values
+y_max = nds.coords['y'].dropna(dim='y').max().values
 print(x_min,x_max,y_min,y_max)
 
 from shapely.geometry import box
 congonhas_bounds = box(x_min,y_min,x_max,y_max)
 
+#%%
 # CORTAR AS RUAS DO OPEN STREET MAPS PARA OS LIMITES
 gdf_clipped = gdf.clip(congonhas_bounds)
 
 del gdf
 
 #%%
+fig,ax = plt.subplots()
 
-gdf_clipped.plot()
+nds.color.isel(time=1).plot(ax=ax)
+gdf_clipped.plot(ax=ax)
 
 
 #%%
